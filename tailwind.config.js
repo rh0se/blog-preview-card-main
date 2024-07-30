bf10271a12c769140b0d7ac60023b78b2ba81620
rh0se <momirhoda@gmail.com>
2024-07-30 20:26:10 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        yellow: "hsl(47, 88%, 63%)",
+        white:" hsl(0, 0%, 100%)",
+        gray500: "hsl(0, 0%, 42%)",
+        gray950: "hsl(0, 0%, 7%)"
+      }
+    },
+  },
+  plugins: [],
+}
+
