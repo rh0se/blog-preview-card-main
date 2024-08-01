4fd46e02bdc418903fd9ed5c6c1e2a95d6f1d432
rh0se <momirhoda@gmail.com>
2024-08-01 13:30:37 +0100

    adding tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      height: {
+        '128': '28rem'
+      },
       colors: {
         yellow: "hsl(47, 88%, 63%)",
         white:" hsl(0, 0%, 100%)",
